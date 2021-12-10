7908dd3bc49710b2d9f5e5a2cb799dcd34588794
apurvakushwaha <ishu.kush@gmail.com>
2021-12-10 20:42:32 +0530

    Added extent reports

--- src/test/java/StepDefinition/Hook.java
+++ src/test/java/StepDefinition/Hook.java
@@ -48,7 +48,7 @@ public class Hook extends BaseUtil {
         //Creating ChromeOptions instance
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setHeadless(true);
-        chromeOptions.addArguments("window-size=1200,1100");
+        chromeOptions.addArguments("window-size=1920,1080");
         chromeOptions.addArguments("--disable-notifications");
         chromeOptions.addArguments("--disable-dev-shm-usage");
 
