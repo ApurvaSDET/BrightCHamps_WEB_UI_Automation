09f54fdd2a67c5d1213326984f001f768299f867
apurvakushwaha <ishu.kush@gmail.com>
2022-01-03 19:41:00 +0530

    Changes done in Refferal test case

--- src/test/java/StepDefinition/StudentTitlePage.java
+++ src/test/java/StepDefinition/StudentTitlePage.java
@@ -699,18 +699,23 @@ public class StudentTitlePage extends BaseUtil {
     }
 
     @When("User opens a new tab and paste copied link in it")
-    public void user_opens_a_new_tab_and_paste_copied_link_in_it() throws IOException, UnsupportedFlavorException {
+    public void user_opens_a_new_tab_and_paste_copied_link_in_it(){
 
-        //Copying the Copied text on clipboard and getting the copied text
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        Object CopiedURL = clipboard.getData(DataFlavor.stringFlavor);
+        //*************Copying the Copied text on Email field and getting the copied text****************
+
+        //Reusing methods to navigate to Email field and pasting the copied link on Email text input
+        Clicks_on_Profile_button();
+        user_clicks_logout_button();
+        user_is_at_student_portal();
+        user_clicks_on_login_with_password_cta();
+        user_is_at_login_with_password_screen();
 
         // Pasting the Copied link using Cmd +V
-        //driver.findElement(By.xpath(valueForTheGivenKey("Email_field"))).sendKeys(Keys.CONTROL + "v");
+        driver.findElement(By.xpath(valueForTheGivenKey("Email_field"))).sendKeys(Keys.CONTROL + "v");
 
 
         //Storing the String value in a local variable
-        // String PastedURL = driver.findElement(By.xpath(valueForTheGivenKey("Email_field"))).getAttribute("value");
+        String PastedURL = driver.findElement(By.xpath(valueForTheGivenKey("Email_field"))).getAttribute("value");
 
         //Opening a new tab
         openNewTab();
@@ -720,18 +725,15 @@ public class StudentTitlePage extends BaseUtil {
 
         // Executing rest of the logic in try catch block after exception is thrown
         try {
-            driver.get(CopiedURL.toString());
-            //driver.get(PastedURL);
+            driver.get(PastedURL);
         } catch (TimeoutException e) {
             // Ignore the exception.
         }
 
         //Validating if new tab is opened with referral URL
-        wait.until(ExpectedConditions.urlToBe(CopiedURL.toString()));
-        //wait.until(ExpectedConditions.urlToBe(PastedURL));
+        wait.until(ExpectedConditions.urlToBe(PastedURL));
         Referral_URL_from_Paste = driver.getCurrentUrl();
         Assert.assertTrue(Referral_URL_from_Paste.contains("referral-demo"));
-
     }
 
     @Then("Verify Same URL should be there which is associated with Book Free Trial CTA")
